6060dd8014db949e03db3a6070b1fca460d7a015
Kauê Murakami <kauetmurakami@gmail.com>
2020-06-09 22:21:11 -0300

    getmain, getprovider, getrepository

--- teste.dart
+++ teste.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-class HomePage extends StatelessWidget {
-//final HomeRepository repository = HomeRepository(apiClient: apiClient(httpClient: http.Client()));
-
-@override
-Widget build(BuildContext context) {
-
-return Scaffold(
-appBar: AppBar(title: Text('Home')),
-body: Container(
-   child: GetX<Home>(
-     builder: (_){
-       return Container();
-     }
-    ),
-  ),
-);
-}
+import 'routes/routes.dart';
+import 'pages/home/home.dart';
+void main() {
+     runApp(
+      GetMaterialApp(
+       namedRoutes: MyRoutes.routes,
+       debugShowCheckedModeBanner: false,
+       home: HomePage(),
+       )
+    );
+  }
