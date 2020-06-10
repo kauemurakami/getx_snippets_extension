20806d09a4f76b39cdf31c5346dd1e9c906b6f65
Kauê Murakami <kauetmurakami@gmail.com>
2020-06-10 17:35:23 -0300

    refatorando, criando estrutura

---
+++ examples/example/lib/src/controller/home/my_controller.dart
@@ -0,0 +1,14 @@
+import 'package:example/src/data/repository/my_repository.dart';
+import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+
+class MyController extends RxController {
+
+  final MyRepository repository;
+  MyController({@required this.repository}) : assert(repository != null);
+
+  final _obj = ''.obs;
+
+  set obj(value) => this._obj.value = value;
+  get obj => this._obj.value;
+}
